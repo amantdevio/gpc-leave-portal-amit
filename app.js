8b1699231f2bdea167246a9fbea0c153f9191518
Aman Tiwari <amantdevio@gmail.com>
2026-02-15 22:26:18 +0530

    liitle changes on app.js

--- app.js
+++ app.js
@@ -36,5 +36,5 @@ app.post('/update', (req, res) => {
     res.redirect('/admin-panel');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 app.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));
